cfdf9349b086556eb845a68a38494bcfaf578f6e
Benedikt Schnatterbeck <benediktms@hey.com>
2023-04-11 15:32:20 +0100

    Add project progress

---
+++ Source/CryptRaider/Grabber.cpp
@@ -0,0 +1,124 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Grabber.h"
+
+#include "DrawDebugHelpers.h"
+#include "Engine/World.h"
+
+// Sets default values for this component's properties
+UGrabber::UGrabber()
+{
+  // Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+  // off to improve performance if you don't need them.
+  PrimaryComponentTick.bCanEverTick = true;
+}
+
+// Called when the game starts
+void UGrabber::BeginPlay()
+{
+  Super::BeginPlay();
+}
+
+// Called every frame
+void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
+{
+  Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+  UPhysicsHandleComponent *PhysicsHandle = GetPhysicsHandle();
+
+  if (PhysicsHandle == nullptr)
+  {
+    return;
+  }
+
+  if (PhysicsHandle->GetGrabbedComponent() != nullptr)
+  {
+    FVector TargetLocation = GetComponentLocation() + GetForwardVector() * GrabDistance;
+    PhysicsHandle->SetTargetLocationAndRotation(TargetLocation, GetComponentRotation());
+  }
+}
+
+void UGrabber::Grab()
+{
+  UPhysicsHandleComponent *PhysicsHandle = GetPhysicsHandle();
+
+  if (PhysicsHandle == nullptr)
+  {
+    return;
+  }
+
+  FHitResult HitResult;
+  bool HasHit = HasGrabbedInReach(HitResult);
+
+  if (HasHit)
+  {
+    UPrimitiveComponent *HitComponent = HitResult.GetComponent();
+    HitComponent->WakeAllRigidBodies();
+
+    DrawDebugSphere(GetWorld(), HitResult.ImpactPoint, 10, 10, FColor::Green, false, 5);
+
+    PhysicsHandle->GrabComponentAtLocationWithRotation(
+        HitComponent,
+        NAME_None,
+        HitResult.ImpactPoint,
+        GetComponentRotation());
+
+    AActor *HitActor = HitResult.GetActor();
+  }
+}
+
+void UGrabber::Release()
+{
+  UPhysicsHandleComponent *PhysicsHandle = GetPhysicsHandle();
+  if (PhysicsHandle == nullptr)
+  {
+    return;
+  }
+
+  UPrimitiveComponent *GrabbedComponent = PhysicsHandle->GetGrabbedComponent();
+
+  if (GrabbedComponent != nullptr)
+  {
+    GrabbedComponent->WakeAllRigidBodies();
+    PhysicsHandle->ReleaseComponent();
+  }
+}
+
+UPhysicsHandleComponent *UGrabber::GetPhysicsHandle() const
+{
+  UPhysicsHandleComponent *Result = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
+
+  if (Result == nullptr)
+  {
+    UE_LOG(LogTemp, Warning, TEXT("No Physics Handle Component found on %s"), *GetOwner()->GetName());
+  }
+
+  return Result;
+}
+
+bool UGrabber::HasGrabbedInReach(FHitResult &OutHitResult) const
+{
+  FVector Start = GetComponentLocation();
+  FVector End = Start + GetForwardVector() * Reach;
+  DrawDebugLine(GetWorld(), Start, End, FColor::Red);
+  DrawDebugSphere(GetWorld(), End, 10, 10, FColor::Red, false, 5);
+
+  FCollisionShape CollisionShape = FCollisionShape::MakeSphere(GrabRadius);
+
+  // Do Geometry Sweep with Sphere
+  // See Sweep Line Algorithm
+  bool HasHit = GetWorld()->SweepSingleByChannel(
+      OutHitResult,
+      Start,
+      End,
+      FQuat::Identity,
+      ECC_GameTraceChannel2,
+      CollisionShape);
+
+  if (HasHit)
+  {
+    DrawDebugSphere(GetWorld(), OutHitResult.ImpactPoint, 10, 10, FColor::Green, false, 5);
+  }
+
+  return HasHit;
+}
